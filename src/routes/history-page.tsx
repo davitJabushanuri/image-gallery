107ef1d697909b357ed171c0aa475789ea32ae39
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-29 16:49:05 +0400

    use Gallery component to fetch and display images on main and history pages

--- src/routes/history-page.tsx
+++ src/routes/history-page.tsx
@@ -1,3 +1,18 @@
+import { Gallery } from "@/components/gallery";
+import { History } from "@/components/history";
+import { useState } from "react";
+
 export const HistoryPage = () => {
-  return <div>history</div>;
+  const [query, setQuery] = useState("");
+
+  return (
+    <div
+      style={{
+        paddingInline: "1rem",
+      }}
+    >
+      <History setQuery={setQuery} />
+      <Gallery query={query} />
+    </div>
+  );
 };
--- src/routes/main-page.tsx
+++ src/routes/main-page.tsx
@@ -1,14 +1,15 @@
+import { Gallery } from "@/components/gallery";
 import { Search } from "@/components/search";
 import { useState } from "react";
 
 export const MainPage = () => {
   const [query, setQuery] = useState<string>("");
 
-  console.log(query);
-
   return (
     <div style={{ paddingInline: "1rem" }}>
       <Search setQuery={setQuery} />
+
+      <Gallery query={query} />
     </div>
   );
 };
