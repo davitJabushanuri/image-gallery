271108627c780045cd724d1d674fe622ac71719e
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-03-02 12:43:33 +0400

    set refetchInterval of 5 minutes to tanstack query

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -9,6 +9,7 @@ const queryClient = new QueryClient({
     queries: {
       refetchOnWindowFocus: false,
       staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchInterval: 1000 * 60 * 5, // 5 minutes
     },
   },
 });
